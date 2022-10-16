228ecd299243530b3de298b5a26dbbb2ea78dcb8
mohammed-Tol <tolhabaig008@gmail.com>
2022-10-16 16:07:53 +0530

    withoutLibrary

---
+++ static/javascript/response.js
@@ -0,0 +1,20 @@
+function getBotResponce(input){
+    if(input=="rock"){
+        return "paper";
+    }
+    else if(input=="paper"){
+        return "scissors";
+    }
+    else if(input=="scissors")
+    return "rock";
+     //Sample Reapnce
+    if(input=="hello"){
+        return "hi! How are you doing";
+    }
+    else if(input=="goodbye"){
+        return "Talk To You Later!Have a nice date.";
+    }
+    else{
+        return"I didnt get you!";
+    }
+}
