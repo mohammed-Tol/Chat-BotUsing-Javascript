0dea0527615c7e3a50d9b916aff892a8cd9dfc22
mohammed-Tol <tolhabaig008@gmail.com>
2022-10-19 18:48:12 +0530

    update the response.js

--- static/javascript/response.js
+++ static/javascript/response.js
@@ -1,20 +1,87 @@
 function getBotResponce(input){
-    if(input=="rock"){
-        return "paper";
-    }
-    else if(input=="paper"){
-        return "scissors";
-    }
-    else if(input=="scissors")
-    return "rock";
-     //Sample Reapnce
-    if(input=="hello"){
-        return "hi! How are you doing";
-    }
-    else if(input=="goodbye"){
-        return "Talk To You Later!Have a nice date.";
-    }
-    else{
-        return"I didnt get you!";
-    }
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+
+// 'tell me a story' -> 'tell me story'
+// 'i feel happy' -> 'happy'
+const robot = ["How do you do, fellow human", "I am not a bot"];
+  text = text
+    .replace(/ a /g, " ")
+    .replace(/i feel /g, "")
+    .replace(/whats/g, "what is")
+    .replace(/please /g, "")
+    .replace(/ please/g, "");
+    const trigger = [
+        //0 
+        ["hi", "hey", "hello"],
+        //1
+        ["how are you", "how are things"],
+        //2
+        ["what is going on", "what is up"],
+        //3
+        ["happy", "good", "well", "fantastic", "cool"],
+        //4
+        ["bad", "bored", "tired", "sad"],
+        //5
+        ["tell me story", "tell me joke"],
+        //6
+        ["thanks", "thank you"],
+        //7
+        ["bye", "good bye", "goodbye"]
+        ];
+        
+        const reply = [
+        //0 
+        ["Hello!", "Hi!", "Hey!", "Hi there!"], 
+        //1
+        [
+            "Fine... how are you?",
+            "Pretty well, how are you?",
+            "Fantastic, how are you?"
+          ],
+        //2
+        [
+            "Nothing much",
+            "Exciting things!"
+          ],
+        //3
+        ["Glad to hear it"],
+        //4
+        ["Why?", "Cheer up buddy"],
+        //5
+        ["What about?", "Once upon a time..."],
+        //6
+        ["You're welcome", "No problem"],
+        //7
+        ["Goodbye", "See you later"],
+        ];
+        
+        const alternative = [
+          "Same",
+          "Go on...",
+          "Try again",
+          "I'm listening...",
+          "Bro..."
+        ];
+
+        if (compare(trigger, reply, text)) {
+            product = compare(trigger, reply, text);
+          } else if (text.match(/robot/gi)) {
+            product = robot[Math.floor(Math.random() * robot.length)];
+          } else {
+            product = alternative[Math.floor(Math.random() * alternative.length)];
+          }
+
+          return product;
 }
+function compare(triggerArray, replyArray, text) {
+    let item;
+    for (let x = 0; x < triggerArray.length; x++) {
+      for (let y = 0; y < replyArray.length; y++) {
+        if (triggerArray[x][y] == text) {
+          items = replyArray[x];
+          item = items[Math.floor(Math.random() * items.length)];
+        }
+      }
+    }
+    return item;
+  }
